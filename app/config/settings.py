ee3f3771629a2eaa202c17e6bec74c2f7e4b8f82
chrisaor <pchpch0070@gmail.com>
2018-06-14 09:27:49 +0900

    todo_list app추가 및 django_extensions 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'todo_list.apps.TodoListConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+
 ]
 
 MIDDLEWARE = [
