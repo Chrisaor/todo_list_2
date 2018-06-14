573dec7ed81a951c87559366a73f0e821a7c42e8
chrisaor <pchpch0070@gmail.com>
2018-06-14 09:43:08 +0900

    model추가

--- app/todo_list/models.py
+++ app/todo_list/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class List(models.Model):
+    item = models.CharField(max_length=200)
+    completed = models.BooleanField('completed')
+    added_time = models.DateTimeField(default=timezone.now())
+    due_date = models.DateTimeField(null=True)
