7b7fe135099105ebbaa38d8e3349f8fb2e12da08
chrisaor <pchpch0070@gmail.com>
2018-06-14 09:51:30 +0900

    admin에 List추가 및 timezone수정

--- app/todo_list/models.py
+++ app/todo_list/models.py
@@ -5,5 +5,8 @@ from django.utils import timezone
 class List(models.Model):
     item = models.CharField(max_length=200)
     completed = models.BooleanField('completed')
-    added_time = models.DateTimeField(default=timezone.now())
-    due_date = models.DateTimeField(null=True)
+    added_time = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.item}'
