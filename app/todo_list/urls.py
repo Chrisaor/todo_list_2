6f29b24ebe4afc4aafc836054edbba05789e599f
chrisaor <pchpch0070@gmail.com>
2018-06-14 21:32:10 +0900

    todo_list 완성

--- app/todo_list/urls.py
+++ app/todo_list/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('crossoff/<int:item_id>', views.crossoff, name='crossoff'),
+    path('uncross/<int:item_id>', views.uncross, name='uncross'),
+    path('crossoff_index/<int:item_id>', views.crossoff_index, name='crossoff_index'),
+    path('uncross_index/<int:item_id>', views.uncross_index, name='uncross_index'),
+    path('detail/<int:item_id>', views.detail, name='detail'),
+    path('delete/<int:item_id>', views.delete, name='delete'),
+    path('edit/<int:item_id>', views.edit, name='edit'),
 ]
--- app/todo_list/views.py
+++ app/todo_list/views.py
@@ -1,15 +1,74 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from todo_list.models import List
 
 
 def index(request):
-    items = List.objects.all()
+    if request.method == 'POST':
+        added_item = request.POST['add_item']
+        created_item = List.objects.create(item=added_item, completed=False)
+        items = List.objects.all()
+        context = {
+            'items':items,
+        }
+        return render(request, 'todo_list/index.html', context)
+    else:
+        items = List.objects.all()
+        context = {
+            'items':items,
+        }
+        return render(request, 'todo_list/index.html', context)
+
+def detail(request, item_id):
+    item = List.objects.get(id=item_id)
     context = {
-        'items':items,
+        'item':item,
     }
-    return render(request, 'todo_list/index.html', context)
+    return render(request, 'todo_list/detail.html', context)
+
+def crossoff(request, item_id):
+    item = List.objects.get(id=item_id)
+    item.completed = True
+    item.save()
+    return redirect('detail', item_id)
+
+def uncross(request, item_id):
+    item = List.objects.get(id=item_id)
+    item.completed = False
+    item.save()
+    return redirect('detail', item_id)
+
+def crossoff_index(request, item_id):
+    item = List.objects.get(id=item_id)
+    item.completed = True
+    item.save()
+    return redirect('index')
+
+def uncross_index(request, item_id):
+    item = List.objects.get(id=item_id)
+    item.completed = False
+    item.save()
+    return redirect('index')
 
 def about(request):
     return render(request, 'todo_list/about.html')
+
+def delete(request, item_id):
+    item = List.objects.get(id=item_id)
+    item.delete()
+    return redirect('index')
+
+def edit(request, item_id):
+    if request.method == 'POST':
+        item_title = request.POST['item']
+        item = List.objects.get(id=item_id)
+        item.item = item_title
+        item.save()
+        return redirect('detail', item_id)
+    else:
+        item = List.objects.get(id=item_id)
+        context = {
+            'item':item,
+        }
+        return render(request, 'todo_list/edit.html', context)
